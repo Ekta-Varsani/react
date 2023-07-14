13c3c708500df8abc073ddc3403d504abf577c68
Ekta Varsani <evarsani@elluminatiinc.com>
2023-07-14 11:18:09 +0530

    websocket added

--- common/forms/src/App.js
+++ common/forms/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import BasicForm from './BasicForm';
+import WebSocketDemo from './WebSocket';
 
 function App() {
   return (
     <div className="App">
-      <BasicForm />
+      {/* <BasicForm /> */}
+      <WebSocketDemo />
     </div>
   );
 }
